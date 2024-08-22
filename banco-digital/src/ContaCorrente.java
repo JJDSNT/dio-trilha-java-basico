43b4c757ee355d200a3e17bfd2ab45de7bf93b87
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-21 21:06:48 -0300

    Adiciona funcionalidades a conta corrente

--- banco-digital/src/ContaCorrente.java
+++ banco-digital/src/ContaCorrente.java
@@ -1,14 +1,41 @@
-
 public class ContaCorrente extends Conta {
 
-	public ContaCorrente(Cliente cliente) {
-		super(cliente);
-	}
+    private double limiteChequeEspecial;
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Corrente ===");
-		super.imprimirInfosComuns();
-	}
-	
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    public ContaCorrente(Cliente cliente, double limiteChequeEspecial) {
+        super(cliente);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        double saldoDisponivel = this.saldo + limiteChequeEspecial;
+        if (valor > saldoDisponivel) {
+            throw new IllegalArgumentException("Saldo insuficiente, incluindo limite de cheque especial");
+        }
+        super.sacar(valor);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+
+    @Override
+    public String getTipoConta() {
+        return "Conta Corrente";
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
 }
