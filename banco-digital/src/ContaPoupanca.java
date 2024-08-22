3290cdce64858f696d1f27265b743788bbcd897b
Jaime Dias <jaimejosediasnt@gmail.com>
2024-08-21 21:07:00 -0300

    Atualiza a conta poupanca

--- banco-digital/src/ContaPoupanca.java
+++ banco-digital/src/ContaPoupanca.java
@@ -1,13 +1,17 @@
-
 public class ContaPoupanca extends Conta {
 
-	public ContaPoupanca(Cliente cliente) {
-		super(cliente);
-	}
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupança ===");
-		super.imprimirInfosComuns();
-	}
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        super.imprimirInfosComuns();
+    }
+
+    @Override
+    public String getTipoConta() {
+        return "Conta Poupança";
+    }
 }
